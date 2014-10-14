123d06f99f24a0f02d19b4afbc05b7322d0f9949
Micheal Colhoun <micheal.etain@outlook.com>
2014-10-14 16:10:00 +0100

    added bower

---
+++ components/devices/devices.js
@@ -0,0 +1,24 @@
+// controllers
+//
+
+function DeviceController($scope)
+{
+	$scope.devices = [{
+	name: "iphone",
+	assetTag: "12312",
+	owner: "dev",
+	desc: "iOS 4.2"
+},
+{
+	name: "laptop",
+	assetTag: "12313",
+	owner: "dev",
+	desc: "iOS 4.2"
+},
+{
+	name: "android",
+	assetTag: "12314",
+	owner: "dev",
+	desc: "iOS 4.2"
+}];
+}
